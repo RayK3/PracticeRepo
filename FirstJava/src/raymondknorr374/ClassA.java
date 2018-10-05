d212712833293ece2e18be5b66f05995c1e3c0c4
Raymond Knorr <rdk3@live.ca>
2018-10-05 12:28:53 -0600

    Added some Java Files

---
+++ FirstJava/src/raymondknorr374/ClassA.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package raymondknorr374;
+
+/**
+ * @author knorr20r
+ *
+ */
+public class ClassA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
